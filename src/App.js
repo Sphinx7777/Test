b444c00565156094bb599b0d80a7ffbac36e389b
Sphinx7777 <spamoglot13@gmail.com>
2019-09-07 17:00:00 +0300

    PC

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import './App.css';
+import s from './App.module.scss';
 
 function App() {
   return (
-    <div className="App">
+    <div className={s.appWrapper}>
 
           Learn React
 
